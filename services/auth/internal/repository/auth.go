01246a1b94894f9749ff61c0be7d4adfd53912bf
Ruletk <damir.timergazin@gmail.com>
2024-12-21 01:54:20 +0500

    feat: Created models and repositories for Auth and Session tables. other: Added gorm to go.mod.

--- services/auth/internal/repository/auth.go
+++ services/auth/internal/repository/auth.go
@@ -1,1 +1,65 @@
 package repository
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+// Auth represents an authentication in the database
+type Auth struct {
+	ID           int       `json:"id" gorm:"primaryKey" gorm:"column:id"`
+	Email        string    `json:"email" gorm:"unique" gorm:"index" gorm:"column:email"`
+	PasswordHash string    `json:"password_hash" gorm:"column:password_hash"`
+	Active       bool      `json:"active" gorm:"column:active" gorm:"default:true"`
+	IsSeller     bool      `json:"is_seller" gorm:"column:is_seller" gorm:"default:false"`
+	CreatedAt    time.Time `json:"created_at" gorm:"column:created_at" gorm:"autoCreateTime"`
+	UpdatedAt    time.Time `json:"updated_at" gorm:"column:updated_at" gorm:"autoUpdateTime"`
+	DeletedAt    time.Time `json:"delete_at" gorm:"column:delete_at"`
+}
+
+// AuthRepository represents the repository for the authentication
+type AuthRepository interface {
+	Create(auth *Auth) error
+	GetByEmail(email string) (*Auth, error)
+	GetByID(id int) (*Auth, error)
+	Update(auth *Auth) error
+	Delete(id int) error
+}
+
+type authRepository struct {
+	db *gorm.DB
+}
+
+func NewAuthRepository() AuthRepository {
+	return &authRepository{}
+}
+
+func (a authRepository) Create(auth *Auth) error {
+	return a.db.Create(auth).Error
+}
+
+func (a authRepository) GetByEmail(email string) (*Auth, error) {
+	var auth Auth
+	err := a.db.Where("email = ?", email).First(&auth).Error
+	if err != nil {
+		return nil, err
+	}
+	return &auth, nil
+}
+
+func (a authRepository) GetByID(id int) (*Auth, error) {
+	var auth Auth
+	err := a.db.Where("id = ?", id).First(&auth).Error
+	if err != nil {
+		return nil, err
+	}
+	return &auth, nil
+}
+
+func (a authRepository) Update(auth *Auth) error {
+	return a.db.Save(auth).Error
+}
+
+func (a authRepository) Delete(id int) error {
+	return a.db.Delete(&Auth{}, "id = ?", id).Error
+}
--- services/auth/internal/repository/session.go
+++ services/auth/internal/repository/session.go
@@ -1,1 +1,71 @@
 package repository
+
+import (
+	"auth/pkg/utils"
+	"gorm.io/gorm"
+	"time"
+)
+
+const (
+	// SessionTTL represents the time to live for the session in seconds. By default, it is set to 1 year.
+	SessionTTL = 60 * 60 * 24 * 365
+)
+
+// Session represents a session in the database
+type Session struct {
+	SessionKey string    `json:"session_key" gorm:"primaryKey" gorm:"column:session_key"`
+	UserID     int       `json:"user_id" gorm:"column:user_id"`
+	LastUsed   time.Time `json:"last_used" gorm:"column:last_used"`
+	ExpiresAt  time.Time `json:"expires_at" gorm:"column:expires_at"`
+	CreatedAt  time.Time `json:"created_at" gorm:"column:created_at" gorm:"autoCreateTime"`
+	UpdatedAt  time.Time `json:"updated_at" gorm:"column:updated_at" gorm:"autoUpdateTime"`
+}
+
+func NewSession(userID int) *Session {
+
+	return &Session{
+		SessionKey: utils.GenerateRandomString(64),
+		UserID:     userID,
+		LastUsed:   time.Unix(0, 0),
+		ExpiresAt:  time.Now().Add(time.Second * SessionTTL),
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+	}
+}
+
+// SessionRepository represents the repository for the session
+type SessionRepository interface {
+	Create(session *Session) error
+	Get(sessionKey string) (*Session, error)
+	Delete(sessionKey string) error
+	HardDelete(sessionKey string) error
+}
+
+type sessionRepository struct {
+	db *gorm.DB
+}
+
+func NewSessionRepository() SessionRepository {
+	return &sessionRepository{}
+}
+
+func (s sessionRepository) Create(session *Session) error {
+	return s.db.Create(session).Error
+}
+
+func (s sessionRepository) Get(sessionKey string) (*Session, error) {
+	var session Session
+	err := s.db.Where("session_key = ?", sessionKey).Where("expires_at > ?", time.Now()).First(&session).Error
+	if err != nil {
+		return nil, err
+	}
+	return &session, nil
+}
+
+func (s sessionRepository) Delete(sessionKey string) error {
+	return s.db.Update("expires_at", time.Now()).Where("session_key = ?", sessionKey).Error
+}
+
+func (s sessionRepository) HardDelete(sessionKey string) error {
+	return s.db.Delete(&Session{}, "session_key = ?", sessionKey).Error
+}

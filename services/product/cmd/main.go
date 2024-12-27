84d54ff7e29075d6da47b8fce5f109b718716402
Ruletk <damir.timergazin@gmail.com>
2024-12-27 18:46:55 +0500

    feat: Connected all logic layers.

--- services/product/cmd/main.go
+++ services/product/cmd/main.go
@@ -1,1 +1,49 @@
 package cmd
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"product/internal/api"
+	"product/internal/repository"
+	"product/internal/service"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token", "Cookie"},
+		AllowCredentials: true,
+	}))
+
+	db := &gorm.DB{} // Just for preventing error
+
+	// Initialize repositories
+	categoryRepo := repository.NewCategoryRepository(db)
+	productRepo := repository.NewProductRepository(db)
+	inventoryRepo := repository.NewInventoryRepository(db)
+	// Initialize services
+	categoryService := service.NewCategoryService(categoryRepo)
+	productService := service.NewProductService(productRepo)
+	inventoryService := service.NewInventoryService(inventoryRepo)
+
+	// Initialize APIs
+	productAPI := api.NewProductAPI(inventoryService, productService, categoryService)
+
+	// Register routes
+	public := r.Group("/")
+	publicOnly := r.Group("/")
+	private := r.Group("/")
+
+	productAPI.RegisterPublicRoutes(public)
+	productAPI.RegisterPublicOnlyRoutes(publicOnly)
+	productAPI.RegisterPrivateRoutes(private)
+
+	err := r.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
--- services/product/internal/api/product.go
+++ services/product/internal/api/product.go
@@ -1,23 +1,28 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"product/internal/service"
+)
 
 type ProductAPI struct {
-	// Services here.
+	inventoryService service.InventoryService
+	productService   service.ProductService
+	categoryService  service.CategoryService
 }
 
-func NewProductAPI() *ProductAPI {
-	return &ProductAPI{}
+func NewProductAPI(inventoryService service.InventoryService, productService service.ProductService, categoryService service.CategoryService) *ProductAPI {
+	return &ProductAPI{inventoryService: inventoryService, productService: productService, categoryService: categoryService}
 }
 
-func (api *ProductAPI) registerPublicOnlyRoutes(router *gin.RouterGroup) {
+func (api *ProductAPI) RegisterPublicOnlyRoutes(router *gin.RouterGroup) {
 	// Get product here.
 }
 
-func (api *ProductAPI) registerPublicRoutes(router *gin.RouterGroup) {
+func (api *ProductAPI) RegisterPublicRoutes(router *gin.RouterGroup) {
 	// Get all products here.
 }
 
-func (api *ProductAPI) registerPrivateRoutes(router *gin.RouterGroup) {
+func (api *ProductAPI) RegisterPrivateRoutes(router *gin.RouterGroup) {
 	// Create, update and delete product here.
 }

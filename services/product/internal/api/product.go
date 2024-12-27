87819d2bd6714e560bf7dd798c3fe93115abb720
Ruletk <damir.timergazin@gmail.com>
2024-12-27 18:29:43 +0500

    feat: Added base to routes.

--- services/product/internal/api/product.go
+++ services/product/internal/api/product.go
@@ -1,1 +1,23 @@
 package api
+
+import "github.com/gin-gonic/gin"
+
+type ProductAPI struct {
+	// Services here.
+}
+
+func NewProductAPI() *ProductAPI {
+	return &ProductAPI{}
+}
+
+func (api *ProductAPI) registerPublicOnlyRoutes(router *gin.RouterGroup) {
+	// Get product here.
+}
+
+func (api *ProductAPI) registerPublicRoutes(router *gin.RouterGroup) {
+	// Get all products here.
+}
+
+func (api *ProductAPI) registerPrivateRoutes(router *gin.RouterGroup) {
+	// Create, update and delete product here.
+}

7946d217a69e81ab08801ef831ebb83e7281db1a
yerassylv <bolatkannn@gmail.com>
2025-01-05 18:31:08 +0600

    feat: Added model and repository for product table.

--- services/product/internal/repository/product.go
+++ services/product/internal/repository/product.go
@@ -3,7 +3,14 @@ package repository
 import "gorm.io/gorm"
 
 type Product struct {
-	// Ера. Добавь сюда поля структуры Product
+	ProductID   int64   `json:"product_id" gorm:"primaryKey" gorm:"column:product_id"`
+	CompanyID   int64   `json:"company_id" gorm:"column:company_id"`
+	Name        string  `json:"name" gorm:"column:name"`
+	Description string  `json:"description" gorm:"column:description"`
+	Price       float64 `json:"price" gorm:"column:price"`
+	CategoryID  int64   `json:"category_id" gorm:"column:category_id"`
+	InventoryID int64   `json:"inventory_id" gorm:"column:inventory_id"`
+	DiscountID  int64   `json:"discount_id" gorm:"column:discount_id"`
 }
 
 func (Product) TableName() string {
@@ -11,7 +18,17 @@ func (Product) TableName() string {
 }
 
 type ProductRepository interface {
-	//	Ера. Добавь сюда методы для работы с продуктами
+	GetByID(id int64) (*Product, error)
+	GetByCompanyID(id int64) ([]*Product, error)
+	GetByName(name string) ([]*Product, error)
+	GetByPage(pageSize int, offset int) ([]*Product, error)
+	GetByFilter(pageSize int, offset int, minPrice int, maxPrice int, sort string, keyword string) ([]*Product, error)
+	Create(product *Product) error
+	Update(product *Product) error
+	DeleteByID(id int64) error
+	DeleteAllByName(name string) error
+	DeleteAllByCategoryID(id int64) error
+	DeleteAllByCompanyID(id int64) error
 }
 
 type productRepository struct {
@@ -23,5 +40,87 @@ func NewProductRepository(db *gorm.DB) ProductRepository {
 		db: db,
 	}
 }
+func (p productRepository) GetByID(id int64) (*Product, error) {
+	var product Product
+	err := p.db.Where("product_id = ?", id).First(&product).Error
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
 
-//	Ера. Добавь сюда методы для работы с продуктами
+func (p productRepository) GetByCompanyID(id int64) ([]*Product, error) {
+	var products []*Product
+	err := p.db.Where("company_id = ?", id).Find(&products).Error
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (p productRepository) GetByName(name string) ([]*Product, error) {
+	var products []*Product
+	err := p.db.Where("name = ?", name).Find(&products).Error
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (p productRepository) GetByPage(pageSize int, offset int) ([]*Product, error) {
+	var products []*Product
+	err := p.db.Limit(pageSize).Offset(offset).Find(&products).Error
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (p productRepository) GetByFilter(pageSize int, offset int, minPrice int, maxPrice int, sort string, keyword string) ([]*Product, error) {
+
+	var products []*Product
+	query := p.db.Limit(pageSize).Offset(offset)
+	if minPrice > 0 {
+		query = query.Where("price >= ?", minPrice)
+	}
+	if maxPrice > 0 {
+		query = query.Where("price <= ?", maxPrice)
+	}
+	if keyword != "" {
+		query = query.Where("name LIKE ?", "%"+keyword+"%")
+	}
+	if sort == "asc" {
+		query = query.Order("price asc")
+	} else {
+		query = query.Order("price desc")
+	}
+	err := query.Find(&products).Error
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (p productRepository) Create(product *Product) error {
+	return p.db.Create(product).Error
+}
+
+func (p productRepository) Update(product *Product) error {
+	return p.db.Save(product).Error
+}
+
+func (p productRepository) DeleteByID(id int64) error {
+	return p.db.Where("product_id = ?", id).Delete(&Product{}).Error
+}
+
+func (p productRepository) DeleteAllByName(name string) error {
+	return p.db.Where("name = ?", name).Delete(&Product{}).Error
+}
+
+func (p productRepository) DeleteAllByCategoryID(id int64) error {
+	return p.db.Where("category_id = ?", id).Delete(&Product{}).Error
+}
+
+func (p productRepository) DeleteAllByCompanyID(id int64) error {
+	return p.db.Where("company_id = ?", id).Delete(&Product{}).Error
+}

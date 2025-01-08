2453efe5db078df1390544385640eb52f181c93f
Ruletk <damir.timergazin@gmail.com>
2025-01-09 01:29:29 +0500

    fix!: Returned back to initialize logger to variable. BREAKING CHANGE: Function InitLogger doesn't return logrus.Logger object. Use logging.Logger variable instead.

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -3,77 +3,37 @@ package logging
 import (
 	"github.com/sirupsen/logrus"
 	"os"
-	"path/filepath"
-	"runtime"
-	"time"
 )
 
 type LogConfig struct {
-	Format string // "json" or "text"
-	Level  string // "debug", "info", "warn", "error", "fatal", "panic"
+	Level string // "debug", "info", "warn", "error", "fatal", "panic"
 }
 
-func InitLogger(config LogConfig) *logrus.Logger {
-	logger := logrus.New()
+var Logger *logrus.Logger
 
-	if config.Format == "json" {
-		logger.SetFormatter(&logrus.JSONFormatter{
-			TimestampFormat: "2006-01-02T15:04:05.000Z07:00",
-			FieldMap: logrus.FieldMap{
-				logrus.FieldKeyTime:  "timestamp",
-				logrus.FieldKeyLevel: "level",
-				logrus.FieldKeyMsg:   "message",
-				logrus.FieldKeyFunc:  "caller",
-			},
-		})
-	} else {
-		logger.SetFormatter(&logrus.TextFormatter{
-			FullTimestamp:   true,
-			TimestampFormat: time.RFC3339,
-			FieldMap: logrus.FieldMap{
-				logrus.FieldKeyTime:  "timestamp",
-				logrus.FieldKeyLevel: "level",
-				logrus.FieldKeyFunc:  "caller",
-				logrus.FieldKeyMsg:   "message",
-			},
-		})
-	}
+func InitLogger(config LogConfig) {
+	Logger = logrus.New()
+
+	Logger.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: "2006-01-02T15:04:05.000Z07:00",
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "timestamp",
+			logrus.FieldKeyLevel: "level",
+			logrus.FieldKeyMsg:   "message",
+			logrus.FieldKeyFunc:  "caller",
+		},
+	})
+
+	Logger.SetReportCaller(true)
 
 	level, err := logrus.ParseLevel(config.Level)
 	if err != nil {
-		logger.Warn("Invalid log level, defaulting to 'info'")
+		Logger.Warn("Invalid log level, defaulting to 'info'")
 		level = logrus.InfoLevel
 	}
 
-	_, file, _, _ := runtime.Caller(0) // Текущий файл
-	projectRoot := filepath.Dir(filepath.Dir(file))
+	Logger.SetLevel(level)
+	Logger.SetOutput(os.Stdout)
 
-	logger.SetLevel(level)
-	logger.SetOutput(os.Stdout)
-	logger.AddHook(&CallerHook{
-		ProjectRoot: projectRoot,
-	})
-
-	logger.Info("Logger initialized")
-	return logger
-}
-
-type CallerHook struct {
-	ProjectRoot string
-}
-
-func (hook *CallerHook) Levels() []logrus.Level {
-	return logrus.AllLevels
-}
-
-func (hook *CallerHook) Fire(entry *logrus.Entry) error {
-	if _, file, line, ok := runtime.Caller(8); ok {
-		relativeFile, err := filepath.Rel(hook.ProjectRoot, file)
-		if err != nil {
-			relativeFile = file
-		}
-		entry.Data["caller"] = relativeFile
-		entry.Data["line"] = line
-	}
-	return nil
+	Logger.Info("Logger initialized")
 }

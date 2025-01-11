7ea0fa243c9a0f4d458e8b920404d26e9522e452
Kampo77 <es2010218@gmail.com>
2025-01-11 22:55:30 +0500

    feat: Downgrade React and ReactDOM to version 18, add react-paginate component, and enhance NavBar with filter overlay functionality

--- services/frontend/frontend/src/pages/Home.js
+++ services/frontend/frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Cards from "../components/Cards"; // Adjust the path as necessary
+import Pagination from "../components/Pagination"; // Adjust the path as necessary
 
 function Home() {
     return (
         <div>
             <Cards/>
+            <Pagination/>
         </div>
     );
 }

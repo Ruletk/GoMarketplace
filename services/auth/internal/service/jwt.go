280ccaeb3d00e9ac17b5ed09b9cb49765e8b9af2
Ruletk <damir.timergazin@gmail.com>
2025-01-15 03:15:31 +0500

    feat(auth): Added custom expiration time for jwt tokens. docs(auth): Added comments.

--- services/auth/internal/service/jwt.go
+++ services/auth/internal/service/jwt.go
@@ -1,9 +1,12 @@
 package service
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
 
 type JwtService interface {
-	GenerateToken(payload map[string]interface{}) (string, error)
+	GenerateToken(payload jwt.MapClaims, expires int64) (string, error)
 	GenerateVerificationToken(userId int64) (token string, err error)
 	GeneratePasswordResetToken(userId int64) (token string, err error)
 	ParseToken(token string) (map[string]interface{}, error)
@@ -24,35 +27,49 @@ func NewJwtService(algo jwt.SigningMethod, secret string) JwtService {
 	}
 }
 
-func (j jwtService) GenerateToken(payload map[string]interface{}) (string, error) {
-	token := jwt.NewWithClaims(j.algo, jwt.MapClaims(payload))
+// GenerateToken generates a new token for the user.
+// The token will expire in time specified by the expires parameter.
+// Expire is added to the payload, if current time is 1000 and expires is 100, the token will expire at 1100.
+func (j jwtService) GenerateToken(payload jwt.MapClaims, expires int64) (string, error) {
+	payload["exp"] = jwt.NewNumericDate(time.Now().Add(time.Second * time.Duration(expires)))
+	payload["iat"] = jwt.NewNumericDate(time.Now())
+	payload["nbf"] = jwt.NewNumericDate(time.Now())
+	token := jwt.NewWithClaims(j.algo, payload)
 	return token.SignedString([]byte(j.secret))
 }
 
+// GenerateVerificationToken generates a new verification token for the user.
+// The token will expire in 7 days.
+// Can be checked with IsVerificationToken.
 func (j jwtService) GenerateVerificationToken(userId int64) (token string, err error) {
-	payload := map[string]interface{}{
+	payload := jwt.MapClaims{
 		"userId": userId,
 		"type":   "verification",
 	}
-	token, err = j.GenerateToken(payload)
+	token, err = j.GenerateToken(payload, 3600*24*7) // 7 days
 	if err != nil {
 		return "", err
 	}
 	return token, nil
 }
 
+// GeneratePasswordResetToken generates a new password reset token for the user.
+// The token will expire in 1 day.
+// Can be checked with IsPasswordResetToken.
 func (j jwtService) GeneratePasswordResetToken(userId int64) (token string, err error) {
 	payload := map[string]interface{}{
 		"userId": userId,
 		"type":   "password_reset",
 	}
-	token, err = j.GenerateToken(payload)
+	token, err = j.GenerateToken(payload, 3600*24) // 1 day, for security reasons
 	if err != nil {
 		return "", err
 	}
 	return token, nil
 }
 
+// ParseToken parses a token and returns the claims.
+// If the token is invalid, an error is returned.
 func (j jwtService) ParseToken(token string) (map[string]interface{}, error) {
 	parsedToken, err := jwt.Parse(token, nil)
 	if err != nil {
@@ -65,6 +82,9 @@ func (j jwtService) ParseToken(token string) (map[string]interface{}, error) {
 	return claims, nil
 }
 
+// IsVerificationToken checks if a token is a verification token.
+// If the token is invalid, false is returned.
+// If the token is a verification token, true is returned and the userId is returned.
 func (j jwtService) IsVerificationToken(token string) (isValid bool, userId int64) {
 	claims, err := j.ParseToken(token)
 	if err != nil {
@@ -73,6 +93,9 @@ func (j jwtService) IsVerificationToken(token string) (isValid bool, userId int6
 	return claims["type"] == "verification", int64(claims["userId"].(float64))
 }
 
+// IsPasswordResetToken checks if a token is a password reset token.
+// If the token is invalid, false is returned.
+// If the token is a password reset token, true is returned and the userId is returned.
 func (j jwtService) IsPasswordResetToken(token string) (isValid bool, userId int64) {
 	claims, err := j.ParseToken(token)
 	if err != nil {
